134d9d690eba6d9feb7b15a45437f3baf59e481a
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-06 16:47:02 -0700

    Build views (drafts) for Cart and Profile pages

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,9 @@
+{{-- Profile Page --}}
+<x-layout-main>
+    <x-slot name="title">
+        Profile - UpOn Cart
+    </x-slot>
+    <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
+        <h1 class="font-title font-extralight text-5xl">User Profile</h1>
+    </div>
+</x-layout-main>
