37a80c2a8ac819fed359de947824d073e7838bef
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-06 16:43:44 -0700

    Build view (draft) for Home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+{{-- Home Page --}}
+<x-layout-main>
+    <x-slot name="title">
+        Home - UpOn Cart
+    </x-slot>
+    <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
+        <h1 class="font-title font-extralight text-5xl">Welcome Back, username!</h1>
+    </div>
+</x-layout-main>
