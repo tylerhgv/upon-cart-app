e5e2b6087cfb85cec9353e5c693b2c2df3713b6f
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-06 16:45:56 -0700

    Build views (drafts) for About, Browse, and Contact pages

---
+++ resources/views/browse.blade.php
@@ -0,0 +1,9 @@
+{{-- Browse Page --}}
+<x-layout-main>
+    <x-slot name="title">
+        Browse - UpOn Cart
+    </x-slot>
+    <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
+        <h1 class="font-title font-extralight text-5xl">Browse Products</h1>
+    </div>
+</x-layout-main>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+{{-- Contact Page --}}
+<x-layout-main>
+    <x-slot name="title">
+        Contact - UpOn Cart
+    </x-slot>
+    <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
+        <h1 class="font-title font-extralight text-5xl">Contact & FAQ</h1>
+    </div>
+</x-layout-main>
