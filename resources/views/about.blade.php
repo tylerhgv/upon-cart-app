1d29d30a28182df6825d3ed13ca20b209ac291a0
rutuja2413 <67887885+rutuja2413@users.noreply.github.com>
2021-06-12 20:28:42 -0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
         <h1 class="font-title font-extralight text-5xl">About Us</h1>
     </div>
     
-    <div class="about us section">
+    <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
     <p>Some text about who we are and what we do.</p>
     <p>Resize the browser window to see that this page is responsive by the way.</p>
     </div>
