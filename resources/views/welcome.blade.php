7719071650ef7867918e65c02d3a07377fd514c9
AseesDhanoa <85143210+AseesDhanoa@users.noreply.github.com>
2021-06-13 21:46:45 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,34 @@
                 </div>
             </div>
         </div>
+
+        <div class="mw6 center pa3 sans-serif">
+
+            <h1 class="mb4">Users</h1>
+
+
+
+            @foreach($users as $user)
+
+            <div class="pa2 mb3 striped--near-white">
+
+                <header class="b mb2">{{ $user->name }}</header>
+
+                <div class="pl2">
+
+                    <p class="mb2">id: {{ $user->id }}</p>
+
+                    <p class="mb2">email: {{ $user->email }}</p>
+
+                    <p class="mb2">details: {{ $user->remember_token }}</p>
+
+                </div>
+
+            </div>
+
+            @endforeach
+
+        </div>
+
     </body>
 </html>
