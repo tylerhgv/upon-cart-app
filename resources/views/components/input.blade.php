939052f37daab902e22fafbad47fe835d4a370c2
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-15 09:29:06 -0700

    Create form-related components

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,7 @@
+{{-- --------------------- --}}
+{{-- Input Field Component --}}
+{{-- --------------------- --}}
+
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
