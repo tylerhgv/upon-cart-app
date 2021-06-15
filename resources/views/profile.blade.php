e94d05ede60a70d43018a34e59e71ac2d0d58fa9
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-15 09:40:22 -0700

    Update views for the main pages

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,12 @@
+{{-- ------------ --}}
 {{-- Profile Page --}}
-<x-layout-main>
+{{-- ------------ --}}
+
+<x-main-layout currentActive="Profile">
     <x-slot name="title">
         Profile - UpOn Cart
     </x-slot>
     <div class="flex justify-center items-center text-center pt-20 pb-5 mx-80 border-b-4 border-accent-dark">
         <h1 class="font-title font-extralight text-5xl">User Profile</h1>
     </div>
-</x-layout-main>
+</x-main-layout>
