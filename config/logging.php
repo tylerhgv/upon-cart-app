24e13ec623431470757eee71a1526e316e1c764f
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-15 10:43:07 -0700

    Change logging to use errorlog for Heroku

--- config/logging.php
+++ config/logging.php
@@ -38,13 +38,13 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+            // 'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
         ],
 
         'daily' => [
