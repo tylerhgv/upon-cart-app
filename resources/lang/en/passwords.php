cf44101a724c44a969b9ba5ab0a2d738aa8eae04
Tyler Hoang <tung.hoangviet@gmail.com>
2021-06-15 09:34:12 -0700

    Update error messages

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,11 @@ return [
     |
     */
 
+    'email' => "We can't find a user with that email address.",
     'reset' => 'Your password has been reset!',
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "We can't find a user with that username.",
 
 ];
