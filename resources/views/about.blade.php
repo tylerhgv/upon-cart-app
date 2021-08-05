c0ef4cc84f5b2d84f8dc04992e7d0327acd8c41d
AseesDhanoa <85143210+AseesDhanoa@users.noreply.github.com>
2021-08-05 15:36:32 -0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
         <p style="text-align:center">
             Some text about who we are and what we do. Resize the browser window to see that this page is responsive by the way.
             Some text about who we are and what we do.
-            Resize the browser window to see that this page is responsive by the way. Test.
+            Resize the browser window to see that this page is responsive by the way.
         </p>
     </div>
 </x-main-layout>
